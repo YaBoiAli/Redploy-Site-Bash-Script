2b6b90726d9f289fc8489722e2806d30a10e2231
Muzzammil Nawab <90220760+YaBoiAli@users.noreply.github.com>
2024-07-18 15:22:13 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# Kill all existing tmux sessions
-tmux kill-server
+cd /root/mlh-portfolio
 
-# Navigate to the project directory
-cd C:\Users\alina\Meta-Intern\portfolio-VPS\meta-portfolio\mlh-portfolio
-
-# Update the Git repository
 git fetch && git reset origin/main --hard
-
-# Enter the Python virtual environment and install dependencies
-source venv/bin/activate
+source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-
-# Start a new detached tmux session with the Flask server
-tmux new-session -d -s flask-server 'source venv/bin/activate && cd /path/to/your/project && flask run'
-
-echo "Redeployment completed successfully."
+sudo systemctl restart myportfolio.service
