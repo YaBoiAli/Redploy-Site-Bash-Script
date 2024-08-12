159a5bc43184ccbe5a81a2b7c5eb882486909d74
YaBoiAli <alinawab9830@gmail.com>
2024-08-12 04:41:30 +0400

    VPS update

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Kill all existing tmux sessions
-tmux kill-server
-
 # Navigate to the project directory
-cd C:\Users\alina\Meta-Intern\portfolio-VPS\meta-portfolio\mlh-portfolio
+cd /mlh-portfolio
 
-# Update the Git repository
+# Fetch the latest changes from the main branch and reset
 git fetch && git reset origin/main --hard
 
-# Enter the Python virtual environment and install dependencies
-source venv/bin/activate
-pip install -r requirements.txt
+# Spin down the Docker containers to prevent memory issues
+docker compose -f docker-compose.prod.yml down
 
-# Start a new detached tmux session with the Flask server
-tmux new-session -d -s flask-server 'source venv/bin/activate && cd /path/to/your/project && flask run'
-
-echo "Redeployment completed successfully."
+# Build and spin up the Docker containers
+docker compose -f docker-compose.prod.yml up -d --build
